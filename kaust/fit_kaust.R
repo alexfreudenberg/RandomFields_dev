2aef8ee98dde4abae62e53f840151db88adcc637
afreuden@uni-mannheim.de <ma_afreuden@uc2n997.localdomain>
2021-01-10 19:09:28 +0100

    Update kaust fit

--- kaust/fit_kaust.R
+++ kaust/fit_kaust.R
@@ -1,20 +1,17 @@
 library(RandomFields)
-library(optimParallel)
-RFoptions(pivot=PIVOT_NONE,printlevel=6, optimiser="lbfgs", cores = 12,printlevel = 6)
-cl <- makeCluster(9)     # set the number of processor cores
-setDefaultCluster(cl=cl) # set 'cl' as default cluster
+RFoptions(pivot=PIVOT_NONE,printlevel=6, reoptimise=F, optimiser="optim", cores = 12,printlevel = 6)
 
 
-for(i in 1:16){
-  df<-read.csv(paste0("~/Documents/kaust/Sub-competition_1a/dataset",i,"_training.csv"))
-  df <- df[1:18e3,]  
+for(i in 8:16){
+  df<-read.csv(paste0("~/kaust/Sub-competition_1a/dataset",i,"_training.csv"))
+  #df <- df[1:18e3,]  
   len <- length(df$x)
   split_index <- sapply(1:9, function(n)rep(n,len/9))
   split_df <- split(df[,1:2],split_index)
   split_val <- split(df[,3],split_index)
   print(system.time(model <- RFfit(RMmatern(nu=NA,scale=NA,var=NA) + RMnugget(var=NA),
-                                   x= split_df,data=split_val, sub.methods="plain",optim.control=list(trace=6,maxit=10,lmm=3),spC=F)))
-  saveRDS(model, file = paste0("kaust/models/dataset_",i,".RDS"))
+                                   x= split_df[1:4],data=split_val[1:4], sub.methods="plain",optim.control=list(trace=6,maxit=10,lmm=3),spC=F)))
+  saveRDS(model, file = paste0("~/kaust/models/dataset_",i,".RDS"))
   cat(timestamp())
   cat(rep("\n",10))
 }
