2456bcdd5eee3eddb5ac456efd88cb381c8eb13c
afreuden@uni-mannheim.de <ma_afreuden@uc2n997.localdomain>
2021-01-11 15:39:48 +0100

    Parallel execution

--- kaust/fit_kaust2.R
+++ kaust/fit_kaust2.R
@@ -1,8 +1,9 @@
 #!/usr/bin/env Rscript
 i = as.numeric(commandArgs(trailingOnly=TRUE))
+sink(paste0("~/kaust/log_dataset",i,".txt"))
 
 library(RandomFields)
-RFoptions(pivot=PIVOT_NONE, critical=-1, reoptimise=F, optimiser="optim", cores = 12,printlevel = 7)
+RFoptions(pivot=PIVOT_NONE, critical=-1, reoptimise=F, optimiser="optim", cores = 1,printlevel = 7)
 
 
   df<-read.csv(paste0("~/kaust/Sub-competition_1a/dataset",i,"_training.csv"))
@@ -13,6 +14,7 @@ RFoptions(pivot=PIVOT_NONE, critical=-1, reoptimise=F, optimiser="optim", cores 
   split_val <- split(df[,3],split_index)
   print(system.time(model <- RFfit(RMmatern(nu=NA,scale=NA,var=NA) + RMnugget(var=NA),
                                    x= split_df[1:4],data=split_val[1:4], sub.methods="plain",optim.control=list(trace=6,maxit=10,lmm=3),spC=F)))
-  saveRDS(model, file = paste0("~/kaust/models/dataset_",i,".RDS"))
+  saveRDS(model, file = paste0("~/kaust/models/dataset_update_",i,".RDS"))
   cat(timestamp())
   cat(rep("\n",10))
+sink(NULL)
